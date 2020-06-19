f2f1418a916123f260f4481a1f6c589a9e5a2ec2
HernandezM-dev <HernandezM.dev@gmail.com>
2020-06-19 11:55:03 -0400

    completed questions and completes arrays-callbacks portion

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,7 +21,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(animal => {return displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}.`)});
 console.log(displayNames);
+
 
 /* Request 2: .map()
 
@@ -29,7 +31,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(animals => {animals.toLowerCase})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
